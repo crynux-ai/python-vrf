4bd184018c9957e02698a2ba27591a847b0a5025
iwehf <henry.lee@crynux.ai>
2024-11-30 12:25:31 +0800

    support auto run gopy build command in pip install

--- build/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gopy build --rename=true --no-make=true -vm=python3 -output=vrf vrf
-cp build/__init__.py vrf/__init__.py
--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
+import shutil
+
 import setuptools
+from setuptools import Command
+from setuptools.command.build import build
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,6 +15,32 @@ class BinaryDistribution(setuptools.Distribution):
         return True
 
 
+class BuildGoPy(Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self) -> None:
+        self.spawn(
+            [
+                "gopy",
+                "build",
+                "--rename=true",
+                "--no-make=true",
+                "-vm=python3",
+                "-output=vrf",
+                "vrf",
+            ]
+        )
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        src_init_file = os.path.join(dir_path, "__init__.py")
+        dst_init_file = os.path.join(dir_path, "vrf", "__init__.py")
+        shutil.copyfile(src_init_file, dst_init_file)
+
+build.sub_commands.insert(0, ("build_gopy", None))
+
 setuptools.setup(
     name="vrf",
     version="0.1.0",
@@ -17,7 +49,8 @@ setuptools.setup(
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(exclude=["build"]),
+    packages=["vrf"],
+    package_data={"vrf": ["*"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
@@ -25,4 +58,5 @@ setuptools.setup(
     ],
     include_package_data=True,
     distclass=BinaryDistribution,
+    cmdclass={"build_gopy": BuildGoPy},
 )
